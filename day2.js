3dc2b0834ecf2cd2713e7e0c344de73d7be08f2d
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2024-07-22 01:15:45 +0530

    day2

---
+++ day2.js
@@ -0,0 +1,75 @@
+//Activity 1 : Arithematic operations
+//T1
+let a=15
+let b=25
+let sum=a+b
+console.log(sum);
+//T2
+let c=15
+let d=25
+let sub=c-d
+console.log(sub);
+//T3
+let e=15
+let f=25
+let mul=e*f
+console.log(mul);
+// T4
+let g=15
+let h=25
+let div=g/h
+console.log(div);
+// T5
+let i=15
+let j=25
+let rem=j%i
+console.log(rem);
+
+//Activity 2:Assignment operetors
+//T6
+let greetings ='hello'
+greetings+= ' world'
+console.log(greetings);
+// T7
+let value=100
+value-=20
+console.log(value);
+
+//Activity 3 :comparison Operator
+//T8
+let p=20
+let q=21
+let greaterthan=p>q
+console.log(greaterthan);
+console.log(2<3);
+//T9
+console.log(5>=6);
+console.log(5>=5);
+console.log(5<=5);
+console.log(4<=5);
+//T 10
+console.log(4==4);
+console.log(4===4);
+console.log(4=='4');
+console.log(4==="4");
+
+//Activity 4:Logical operators
+// T 11
+let x=20
+let y=25
+console.log((x>1)&&(y>5));//when botha re true then output comes true
+//T 12
+let m=40
+let n=45
+console.log(n===5||m>2);//when both are fals then the outut comes false
+//T 13
+let k=true
+console.log(!k);
+
+//Activity 5:Ternary Operator
+//T 14
+let num=14
+let type=num>=0?'positive':'negative'
+console.log(type);
+
+// ------the end------
