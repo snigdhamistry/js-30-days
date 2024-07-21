0af75b7cf8292202b780ba9477c1da35972cc866
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2024-07-21 23:54:37 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,31 @@
+var number=12;
+console.log(number);
+console.log(typeof(number));
+let string='All izz well'
+console.log(string);
+console.log(typeof(string));
+const bolvalue=true
+console.log(bolvalue );
+console.log(typeof(bolvalue));
+let userinfo ={
+    username:"snigdha",
+    email:'abc@gmail.com'
+}
+console.log(userinfo);
+console.log(typeof(userinfo));
+let myarr=[1,2,3,4,5,]
+console.log(myarr);
+console.log(typeof(myarr));
+
+//declear a veriable using let ,assign it an initial value reassign a new value and log both value to the console 
+let veriable=123
+console.log(veriable);//123
+veriable=321
+console.log(veriable);//321
+
+//doing the same thing using const
+const newveriable=123
+console.log(newveriable);
+newveriable=321
+console.log(newveriable);//we can not reassign value inside the constant variable
+
