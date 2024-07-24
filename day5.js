b08a733191ede02ea25143fad972a97af0a01126
Snigdha Mistry <150170898+snigdhamistry@users.noreply.github.com>
2024-07-24 23:52:22 +0530

    day5

---
+++ day5.js
@@ -0,0 +1,44 @@
+//Activity 1
+//T1
+function num(int) {
+    if (int%2===0) {
+        console.log(`this number ${int}is even `)
+    } else {
+        console.log(`this number ${int}is odd`);
+    }
+}
+num(25)
+//T2
+function squareofanum(number) {
+    let square=number*number
+    return square
+}
+console.log(squareofanum(2));
+//Activity 2:
+//T3
+function max(number1,number2) {
+    if (number1>number2) {
+        return number1;
+    } else {
+        return number2;
+    }
+}
+console.log(`maximun number is ${max(20,1)}`); 
+//T4
+function concatinate(string1,strings) {
+    return string1+strings
+}
+console.log(`hi there, ${concatinate('myself ' ,'Snigdha')}`);
+
+//Activity 4:
+//T5
+
+
+
+
+
+
+
+
+
+
